98ee454ef7653971e0ed367f3974640e70dcb4be
SBTHDR <subrata.halder13d@gmail.com>
2024-09-16 16:37:56 +0600

    Chore: Container Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
     "./public/js/**/*.js",
   ],
   theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px'
+    },
+    container: {
+      center: true,
+      padding: '1rem'
+    },
     extend: {
       fontFamily: {
         poppins: "'Poppins', sans-serif",
